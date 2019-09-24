a6d507fba2019a9c8b826261ee5e0a8f1322deee
ingan <isabelngan27@gmail.com>
2019-09-24 14:39:32 -0700

    complete results page

---
+++ public/js/quiz.js
@@ -0,0 +1,6 @@
+// on-click listener for submiting initial quiz
+$("").on("click", () => {
+    // update table that they passed the quiz or failes
+    // if failed, a modal will show and a "fake" suiter will come up
+    // if passed reroute to initial about you quiz
+})
