d3660134a1478c6a3e82f2b5a1f1834dbe6a0bba
ingan <isabelngan27@gmail.com>
2019-09-25 20:30:03 -0700

    adding css

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # CLICK
-#### A modern day dating app that start with the foundation of common knowledge
+#### A modern day dating app that starts with the foundation of common knowledge
+
+<br>
+<img src="https://media.giphy.com/media/26BRED0APH6fRa1sk/giphy.gif">
+<br><br>
+
+## About Click
+
+#### Finding love is hard, even for celebrities. Celebrities are experts in thier field, but are only a fraction of the true talent in the world. We want to connect experts, both celebrities and connoisseurs in thier an industry, together.
+
+## User Journey
+<br>
+<img src="https://media.giphy.com/media/1xmlYh65ZA8rOjKwr3/giphy.gif">
+<br>
+
+* User will log in or sign up to CLICK
+* They will take a preliminary quiz of their knowledge
+* Once they pass the quiz with flying colors, users will be able to fill out their profile and set their preferences
+* Users can send connect requests and explore profiles, beginning their journey of love!
+
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,32 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Tech.findAll({}).then(function(dbExamples) {
-      res.render("initial-quiz", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.render('aboutapp', { layout: 'main.handlebars' });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Tech.findAll({}).then(function(dbExamples) {
+  //     res.render("initial-quiz", {layout: 'inital.handlebars'},{
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
