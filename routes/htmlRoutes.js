1dbff5766ad9a3432291a33d848c2423781247f0
syeda zaidi <zaidisyeda03@gmail.com>
2019-09-25 20:11:33 -0700

    added initial get routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,36 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Tech.findAll({}).then(function(dbExamples) {
-      res.render("initial-quiz", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+module.exports = function (app) {
+
+  // load the initial quiz page
+  app.get("/lovequiz", function (req, res) {
+    res.render("initial-quiz", ({ layout: "initial.handlebars" }));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  //load the first page for signin
+  app.get("/", function (req, res) {
+    res.render("signin", ({ layout: 'initial.handlebars' }));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/signup", function (req, res) {
+    res.render("signup", ({ layout: 'initial.handlebars' }));
   });
+
+  
+
+
+
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  // // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
