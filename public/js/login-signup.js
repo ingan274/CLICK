ec0175f46b3806c9e620c4e4e25cc79a84feb4fe
ingan <isabelngan27@gmail.com>
2019-09-28 14:40:11 -0700

    adding results

--- public/js/login-signup.js
+++
@@ -1,9 +0,0 @@
-// on-click listener for log in
-$("").on("click", () => {
-    // reroute to initial quiz or results page
-})
-
-// on-click listener for sign up
-$("").on("click", () => {
-    // reroute to initial quiz
-})
