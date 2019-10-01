a688ecc9169409d712b8dd75161ff3a058c0f594
ingan <isabelngan27@gmail.com>
2019-09-30 23:45:28 -0700

    button set up

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
     // on-click listener for submiting initial quiz
-    $("").on("click", () => {
+    $(".initial-quiz-submit").on("click", (event) => {
+        event.preventDefault()
+        var q1 = $(".qa1").data('value');
+        var q2 = $(".qa2").data('value');
+        var q3 = $(".qa3").data('value');
+        var q4 = $(".qa4").data('value');
 
-        // if () {
-        //     location.href = '/profile-setup'
-        // } else {
-
-        // }
+        if (q1 === 'qa1a' && q2 === 'qa2b'  && q3 === 'qa3a'  && q4 === 'qa4a') {
+            location.href = '/profile-setup'
+        } else {
+            $('#modal-alert').modal('show')
+        }
         // update table that they passed the quiz or failes
         // if failed, a modal will show and a "fake" suiter will come up
         // if passed reroute to initial about you quiz
